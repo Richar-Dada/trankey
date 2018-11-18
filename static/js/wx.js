c32ff19de0e6fe56d54571e5589b0bf8e1a122fa
Richar-Dada <631151149@qq.com>
2018-11-18 22:05:09 +0800

    Push  壮阳

---
+++ static/js/wx.js
@@ -0,0 +1,13 @@
+var arr_wx = [
+['zxcn38',''],['zxcn38',''],['zxcn38',''],['zxcn38',''],['zxcn38',''],['zxcn38','']
+]
+
+var wx_index = Math.floor((Math.random()*arr_wx.length));
+
+
+
+var stxlwx = arr_wx[wx_index];
+var wx = ""+arr_wx[wx_index];
+var img = arr_wx[wx_index]+ ".jpg";
+var wx_img = "<img width='168' height='168' src='" + img + "'>";
+
